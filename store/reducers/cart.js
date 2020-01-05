f8fcada39f104d22d016b5caad6934a49c3c3128
tikken <big_nick@gmail.com>
2020-01-05 15:17:10 +0300

    cart reducer

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,3 +1,36 @@
-const initialState = {
+import { ADD_TO_CART } from "../actions/cart";
+import CartItem from '../../models/cart-item';
 
+const initialState = {
+    items: {},
+    totalAmout: 0
+};
+
+export default (state = initialState, action) => {
+    switch(action.type) {
+        case ADD_TO_CART:
+            const addedProduct = action.product;
+            const prodPrice = addedProduct.price;
+            const prodTitle = addedProduct.title;
+
+            let updatedOrNewCartItem;
+
+            if(items[addedProduct.id]) {
+                //already have item in the cart
+                const updatedOrNewCartItem = new CartItem(
+                        state.items[addedProduct.id].quantity + 1,
+                        prodPrice,
+                        prodTitle,
+                        state.items[addedProduct.id].sum + prodPrice
+                    );
+            } else {
+                const updatedOrNewCartItem = new CartItem(1, prodPrice, prodTitle, prodPrice);
+            }
+
+            return {
+                ...state, items: { ...state.items, [addedProduct.id]: updatedOrNewCartItem },
+                totalAmout: state.totalAmout + prodPrice
+            }
+    }
+    return state;
 };
